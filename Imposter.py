cf9418e70d09c1579f214910a13b6b1c5f88cce1
MightyT-2 <timmightyt2@gmail.com>
2026-01-30 11:02:04 -0500

    Add More Code

--- Imposter.py
+++ Imposter.py
@@ -1,8 +1,8 @@
 from os import system, listdir
 from platform import system as os
+from random import sample, choice
 
 inputPlayers  = ''
-playerList    = []
 imposterCount = 0
 inputCount    = ''
 dictList      = list(x[:-4] for x in listdir('dictionaries') if x[-4:] == '.txt')
@@ -11,9 +11,13 @@ indexSelector = ''
 userIndex     = 0
 currentFile   = ''
 wordCount     = 0
+imposters     = []
+wordList      = []
 
 class Player:
     playerCount = 0
+    currentWord = ''
+    playerList    = []
 
     def __init__(this, playerName):
         this.name = playerName
@@ -32,6 +36,26 @@ class Player:
     
     def getId(this):
         return this.playerId
+    
+    def getWord(this):
+        if this.isImposter:
+            return "Imposter"
+        else:
+            return Player.currentWord
+    
+    def getName(this):
+        return this.name
+    
+    @classmethod
+    def setWord(cls, newWord):
+        cls.currentWord = newWord
+    
+    @classmethod
+    def getNameList(cls):
+        returnList = []
+        for x in cls.playerList:
+            returnList.append(x.getName())
+        return returnList
 
 def clearScreen():
     if os() == 'Windows':
@@ -40,23 +64,23 @@ def clearScreen():
         system('clear')
 
 if __name__ == "__main__":
+    clearScreen()
 
     # Get the player list
     inputPlayers = ''
     while inputPlayers != '!done':
         inputPlayers = input('Enter player name: ')
-        if inputPlayers != '!done':
-            playerList.append(Player(inputPlayers.strip()))
-            print(playerList[len((playerList)) - 1].getId())
+        if inputPlayers not in ('!done', '') + Player.getNameList():
+            Player.playerList.append(Player(inputPlayers.strip()))
     clearScreen()
 
     # Get the imposter count
-    while imposterCount <= 0 or imposterCount > len(playerList) / 2:
+    while imposterCount <= 0 or imposterCount > len(Player.playerList) / 2:
         try:
-            imposterCount = int(input(f'How many imposters do you want (player count: {len(playerList)}): '))
+            imposterCount = int(input(f'How many imposters do you want (player count: {len(Player.playerList)}): '))
             if imposterCount <= 0:
                 print('You need more than 0 imposters!')
-            if imposterCount > len(playerList) / 2:
+            if imposterCount > len(Player.playerList) / 2:
                 print('You need less than half the player count as imposter!')
         except:
             print('Not a valid number!')
@@ -66,9 +90,10 @@ if __name__ == "__main__":
     index         = 0
     indexSelector = ''
     userIndex     = -1
+    print(dictList)
     print('Select a category.')
     for x in dictList:
-        wordCount = len(open('dictionaries\\' + x + '.txt', 'r').read().split('\n'))
+        wordCount = len(open('dictionaries/' + x + '.txt', 'r').read().split('\n'))
         print(f" {index}) {x} ({wordCount})")
         index += 1
     while userIndex <= -1 or userIndex >= index:
@@ -80,4 +105,14 @@ if __name__ == "__main__":
     currentFile = dictList[userIndex]
     clearScreen()
 
-    #
+    # Select word
+    wordList = open('dictionaries/' + currentFile + '.txt', 'r').read().split('\n')
+    Player.setWord(choice(wordList))
+
+    # Set imposters
+    imposters = sample(Player.playerList, imposterCount)
+    for x in imposters:
+        x.setImposter()
+    
+    for x in Player.playerList:
+        print(f'{x.getName()} {x.getWord()}')
